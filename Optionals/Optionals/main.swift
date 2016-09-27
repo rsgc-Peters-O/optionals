f593c5c78f560cbb4f63c1c3586d6869f7e9f5c7
rgordonatrsgc-student <student@russellgordon.ca>
2016-09-26 20:40:59 -0400

    Changed example command line program to introduce 'input-process-output' problem solving model

--- Optionals/Optionals/main.swift
+++ Optionals/Optionals/main.swift
@@ -1,103 +1,114 @@
-//
-//  main.swift
-//  Optionals
-//
-//  Created by Russell Gordon on 2016-02-28.
-//  Copyright © 2016 Royal St. George's College. All rights reserved.
+/*
+ 
+ PROBLEM
+ 
+ Given a number, n, determine whether the number is prime.
+ 
+ A prime number is has no positive divisors other than 1 and itself.
+ 
+ The input, n, can range between 2 and 99999, inclusive.
+  
+ INPUT/OUTPUT EXAMPLES
+ 
+ Example 1
+ ---------
+ What is the number? 16
+ 
+ This is not prime.
 
-// Example #1 – As simple as it gets – get string input, no matter what it is
-//              NOTE: No error checking is performed on input given
-var month : String = ""     // Non-optional data type; will store the provided value eventually
-// Prompt the user
-print("What is the current month? ", terminator: "")
-// Use optional binding to unwrap the optional String data type that is always returned
-// from the readLine function (this function is what takes input from the user)
-if let input = readLine(stripNewline: true) {
-    month = input
-}
-// Print a response (with two line breaks at the end)
-print("The month is \(month)", terminator: "\n\n")
+ Example 2
+ ---------
+ What is the number? 
+ What is the number? 15
+ 
+ This is not prime.
 
+ Example 3
+ ---------
+ What is the number? 7
+ 
+ This is prime.
+ 
+ */
 
-// Example #2 – Ensure string input is given and not just an empty string
-//
-var name : String = ""      // Non-optional data type; will store the provided value eventually
-repeat {
-    // Prompt the user
-    print("What is your name? ", terminator: "")
-    // Unwrap the optional (readLine always returns an optional String data type)
-    if let input = readLine(stripNewline: true) {
-        // Check that the input is not just an empty string
-        if input != "" {
-            // Use this input, it's good
-            name = input
-        } else {
-            // Tell the user what they need to do
-            print("Please enter your name.")
+/*
+ 
+ MAKING A PLAN
+ 
+ A common problem solving model is known as input-process-output.
+ 
+ 1. What are the possible inputs I can expect to receive?
+ 2. How should each type of input be processed?
+ 3. What should the output be in each case?
+ 
+ It can be helpful to organize this into a table. Here's an example:
+ 
+ http://russellgordon.ca/rsgc/2016-17/ics3u/IPO-example.jpg
+ 
+ */
+
+/*
+ 
+ IMPLEMENT THE PLAN
+ 
+ A good way to orgaize your code is to separate your code into the three sections - input, process, output – as much as possible.
+ 
+ The start of a solution is implemented below. Consider all the possible inputs. Can you finish the solution?
+
+ */
+
+// INPUT
+
+// Regular integer, will store the value provided, eventually
+var possiblePrime : Int = 0
+
+// Loop until a valid potential prime is given
+while possiblePrime == 0 {
+    
+    // Show the prompt
+    print("What is the number? ", terminator: "")
+    
+    // Get the user's input
+    var input : String?
+    input = readLine()
+    
+    // Use optional binding to see if the string can be unwrapped (this will happen only if the input is not nil)
+    if let notNilInput = input {
+        
+        // Now we know we have a non-nil string
+        // Try to make the string into an integer (again we can use optional binding)
+        if let inputAsInteger = Int(notNilInput) {
+            
+            // Now we know we have the input as an integer, so assign it to the value we'll work with below, in the "process" section
+            possiblePrime = inputAsInteger
+            
         }
+        
     }
-} while name == ""  // Keep looping until proper input provided
-// Greet the user (with two line breaks at the end)
-print("Nice to meet you, \(name)", terminator: "\n\n")
 
-
-// Example #3 – Get input and attempt to save it in an non-optional variable of type Int
-//              NOTE: No error checking is performed on input given
-var favourite : Int = 0     // Non-optional data type; will store the provided value eventually
-// Prompt the user
-print("What is your favourite positive integer? ", terminator: "")
-// Use optional binding to unwrap the optional String data type that is always returned
-// from the readLine function (this function is what takes input from the user)
-if let input = readLine(stripNewline: true) {
-    // We have a non-nil value, now to try to get it as an Int
-    if let inputAsInteger = Int(input) {
-        favourite = inputAsInteger
-    }
 }
-// Print a response (with two line breaks at the end)
-print("\(favourite) is your favourite integer??? Mine too!", terminator: "\n\n")
 
 
-// Example #4 – Ensure input is given and can be used as an integer
-//
-var age : Int = 0      // Non-optional data type; will store the provided value eventually
-repeat {
-    // Prompt the user
-    print("What is your age? ", terminator: "")
-    // Unwrap the optional (readLine always returns an optional String data type)
-    if let input = readLine(stripNewline: true) {
-        // We have a non-nil value, now to try to get it as an Int
-        if let inputAsInteger = Int(input) {
-            // Use this input, it's good
-            age = inputAsInteger
-        } else {
-            // Tell the user what they need to do
-            print("Please enter an integer value for your age.")
-        }
+// PROCESS
+var prime : Bool = true    // Assume prime unless we find it is not
+
+// Lowest possible input value should be 2
+if possiblePrime != 2 {     // 2 is prime, so we wouldn't need to bother with checks below
+    
+    // Check to see if the number is even
+    if possiblePrime % 2 == 0 {
+        
+        prime = false       // This is an even number (that is not 2) so possiblePrime cannot be prime
+        
     }
-} while age == 0  // Keep looping until proper input provided
-// Print a response (with two line breaks at the end)
-print("Your age is: \(age)", terminator: "\n\n")
-
-// Exercise #1 - Use the code above as an exemplar.
-//               Write code that will guarantee a non-integer value greater than 0 is obtained from the user
-//
-// Begin your code here... test that it works... comment it out when complete.
+    
+}
 
 
-// Exercise #2 - Use the code above as an exemplar.
-//               Write code that will guarantee an integer value between 0 and 100 inclusive is obtained from the user
-//
-// Begin your code here... test that it works... comment it out when complete.
+// OUTPUT
+if prime {
+    print("This is prime.")
+} else {
+    print("This is not prime.")
+}
 
-
-// Exercise #3 - Use the code above as an exemplar.
-//               Write code that will guarantee a string value of "Small", "Medium", or "Large" is obtained (but no other value)
-//
-// Begin your code here... test that it works... comment it out when complete.
-
-
-// Exercise #4 - Use the code above as an exemplar.
-//               Convert one of your programs (written in a playground earlier) to use a proper command line input model.
-//
-// Begin your code here... test that it works.
